0dc51a7edbd349c5a572f0dc76aa10dc65babc0c
DroidModderXtreme <leonardobianco44@gmail.com>
2014-03-04 19:48:57 -0500

    Update com_android_bluetooth_btservice_AdapterService.cpp

--- jni/com_android_bluetooth_btservice_AdapterService.cpp
+++ jni/com_android_bluetooth_btservice_AdapterService.cpp
@@ -910,12 +910,6 @@ static jboolean configHciSnoopLogNative(JNIEnv* env, jobject obj, jboolean enabl
 
     jboolean result = JNI_FALSE;
 
-    if (!sBluetoothInterface) return result;
-
-    int ret = sBluetoothInterface->config_hci_snoop_log(enable);
-
-    result = (ret == BT_STATUS_SUCCESS) ? JNI_TRUE : JNI_FALSE;
-
     return result;
 }
 
